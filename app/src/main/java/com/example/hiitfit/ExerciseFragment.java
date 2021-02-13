c2e94c0b0da79851935812383622adc76db42194
Navneet Singh <navneetsingh6122@gmail.com>
2021-02-13 11:17:19 -0500

    user chest exercise connected with textviews

--- app/src/main/java/com/example/hiitfit/ExerciseFragment.java
+++ app/src/main/java/com/example/hiitfit/ExerciseFragment.java
@@ -14,27 +14,129 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ExerciseFragment extends Fragment {
-    TextView exerciseActivity , chestActivity;
+
+    TextView exerciseActivity, chestActivity , TrapsActivity,Abductors,Abs,Biceps,Calves,ForeArm,Obliques,Triceps,Quads;
+int nb;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =  inflater.inflate(R.layout.fragment_exercises,container,false);
-        exerciseActivity = (TextView)v.findViewById(R.id.shoulder);
-        chestActivity = (TextView)v.findViewById(R.id.chest);
+        exerciseActivity = (TextView)v.findViewById(R.id.shoulder_u);
+        chestActivity = (TextView)v.findViewById(R.id.chest_u);
+        TrapsActivity = (TextView)v.findViewById(R.id.Traps_u);
+        Abductors = (TextView)v.findViewById(R.id.Abductors_u);
+        Abs = (TextView)v.findViewById(R.id.Abs_u);
+        Biceps = (TextView)v.findViewById(R.id.Biceps_u);
+        Calves = (TextView)v.findViewById(R.id.calves_u);
+        ForeArm = (TextView)v.findViewById(R.id.Forearms_u);
+        Obliques = (TextView)v.findViewById(R.id.Obliques_u);
+        Triceps = (TextView)v.findViewById(R.id.Triceps_u);
+        Quads = (TextView)v.findViewById(R.id.Quads_u);
+
         exerciseActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), ShoulderExercise.class);
+                nb = 0;
+                intent.putExtra("ID", nb);
                 startActivity(intent);
             }
         });
-chestActivity.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        Intent che = new Intent(getActivity(),ChestExercise.class);
-        startActivity(che);
-    }
-});
+   chestActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent (getActivity(), ShoulderExercise.class);
+                nb = 1;
+                next.putExtra("ID", nb);
+
+                startActivity(next);
+            }
+        });
+
+
+        TrapsActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ShoulderExercise.class);
+                nb = 2;
+                intent.putExtra("ID",nb);
+                startActivity(intent);
+            }
+        });
+
+        Abductors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ShoulderExercise.class);
+                nb = 3;
+                intent.putExtra("ID",nb);
+                startActivity(intent);
+            }
+        });
+
+        Abs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ShoulderExercise.class);
+                nb = 4;
+                intent.putExtra("ID",nb);
+                startActivity(intent);
+            }
+        });
+        Biceps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ShoulderExercise.class);
+                nb = 5;
+                intent.putExtra("ID",nb);
+                startActivity(intent);
+            }
+        });
+        Calves.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ShoulderExercise.class);
+                nb = 6;
+                intent.putExtra("ID",nb);
+                startActivity(intent);
+            }
+        });
+        ForeArm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ShoulderExercise.class);
+                nb = 7;
+                intent.putExtra("ID",nb);
+                startActivity(intent);
+            }
+        });
+        Obliques.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ShoulderExercise.class);
+                nb = 8;
+                intent.putExtra("ID",nb);
+                startActivity(intent);
+            }
+        });
+        Triceps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ShoulderExercise.class);
+                nb = 9;
+                intent.putExtra("ID",nb);
+                startActivity(intent);
+            }
+        });
+        Quads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ShoulderExercise.class);
+                nb = 10;
+                intent.putExtra("ID",nb);
+                startActivity(intent);
+            }
+        });
         return v;
     }
 }
--- app/src/main/java/com/example/hiitfit/ShoulderExercise.java
+++ app/src/main/java/com/example/hiitfit/ShoulderExercise.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -23,42 +24,66 @@ ArrayList<ShoulderModel> detailList;
 FirebaseFirestore db;
 ShoulderAdapter madapter;*/
 private FirebaseFirestore db = FirebaseFirestore.getInstance();
-private CollectionReference exerciseRef = db.collection("Shoulder");
+private CollectionReference exerciseref;
 
 private ShoulderAdapter madapter;
-
+int number;
+TextView ExerciseName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shoulder_exercise);
+        ExerciseName = (TextView)findViewById(R.id.user_exercise_name);
+        number = getIntent().getExtras().getInt("ID");
+
+        switch(number){
+            case(0):exerciseref = db.collection("Shoulder");
+                ExerciseName.setText("Shoulder");
+                break;
+            case(1):exerciseref = db.collection("Chest");
+                ExerciseName.setText("Chest");
+                break;
+            case(2):exerciseref = db.collection("Traps");
+                ExerciseName.setText("Traps");
+                break;
+            case(3):exerciseref = db.collection("Abductors");
+                ExerciseName.setText("Abductors");
+                break;
+            case(4):exerciseref = db.collection("Abs");
+                ExerciseName.setText("Abs");
+                break;
+            case(5):exerciseref = db.collection("Biceps");
+                ExerciseName.setText("Biceps");
+                break;
+            case(6):exerciseref = db.collection("Calves");
+                ExerciseName.setText("Calves");
+                break;
+            case(7):exerciseref = db.collection("ForeArms");
+                ExerciseName.setText("ForeArms");
+                break;
+            case(8):exerciseref = db.collection("Obliques");
+                ExerciseName.setText("Obliques");
+                break;
+            case(9):exerciseref = db.collection("Triceps");
+                ExerciseName.setText("Triceps");
+                break;
+            case(10):exerciseref = db.collection("Quads");
+                ExerciseName.setText("Quads");
+                break;
+
+        }
+
+
+
+        // exerciseRef =  db.collection("Shoulder");
         setUpRecyclerView();
 
-       /* sh = (RecyclerView)findViewById(R.id.Shoulderrecycler);
-        sh.setLayoutManager(new LinearLayoutManager(this));
 
-        detailList = new ArrayList<>();
-        madapter = new ShoulderAdapter(detailList);
-        sh.setAdapter(madapter);
-        db = FirebaseFirestore.getInstance();
-
-        db.collection("Shoulder").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-            @Override
-            public void onSuccess(QuerySnapshot documentSnapshots) {
-                List<DocumentSnapshot> list = documentSnapshots.getDocuments();
-                for(DocumentSnapshot d:list){
-                    ShoulderModel obj = d.toObject(ShoulderModel.class);
-                    detailList.add(obj);
-                }
-
-                //update adapter
-                madapter.notifyDataSetChanged();
-            }
-        });*/
 
     }
 
     private void setUpRecyclerView() {
-        Query query = exerciseRef.orderBy("Name", Query.Direction.ASCENDING);
+        Query query = exerciseref.orderBy("Name", Query.Direction.ASCENDING);
 
         FirestoreRecyclerOptions<ShoulderModel> options = new FirestoreRecyclerOptions.Builder<ShoulderModel>().
                 setQuery(query,ShoulderModel.class).build();
